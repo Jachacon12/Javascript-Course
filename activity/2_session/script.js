fa9d7480ffa315387e6e5c84fcfe8fff30a20116
Jachacon12 <jorgech.webdeveloper@gmail.com>
2024-05-27 21:19:10 +0200

    feat: missing JS doc added to class

--- activity/2_session/script.js
+++ activity/2_session/script.js
@@ -1,14 +1,31 @@
+/**
+ * @class InvertirCadena
+ * @classdesc This class is used to reverse a string.
+ */
 export class InvertirCadena {
+   /**
+   * @constructor
+   * @description Constructor for the InvertirCadena class. Initializes an empty string to be reversed.
+   */
   constructor() {
+    // Initialize the string to be reversed
     this.cadenaInvertir = '';
   }
 
+   /**
+   * @method reverseString
+   * @description This method is used to reverse the string. If the string is empty, it throws an error. Otherwise, it returns the reversed string.
+   * @throws {Error} Will throw an error if the string to be reversed is empty.
+   * @returns {string} The reversed string.
+   */
   reverseString = () => {
+    // Check if the string to be reversed is empty
     if (this.cadenaInvertir === '') {
+      // Throw an error if the string is empty
       throw new Error('La cadena a invertir no puede estar vacía.');
     }
-
     const cadenaInvertida = this.cadenaInvertir.split('').reverse().join('');
+    // Return the reversed string
     return cadenaInvertida;
   };
 }
